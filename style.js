e808942018e7c86b5b22f942abf83a77f03721bc
NjengaBen <bennjenga2@gmail.com>
2023-08-27 03:41:38 +0300

    fifth

--- style.js
+++ style.js
@@ -1,17 +1,14 @@
 const home = document.getElementById("home");
-const services = document.getElementById("services");
-const craft = document.getElementById("craft");
+const midSection = document.getElementById("mid-section");
 const homelink = document.getElementById("homelink");
 const servicelink = document.getElementById("servicelink");
 
 homelink.addEventListener("click", () => {
-  home.style.display = "flex";
-  services.style.display = "none";
-  craft.style.display = "none";
+  home.style.display = "grid";
+  midSection.style.display = "none";
 });
 
 servicelink.addEventListener("click", () => {
   home.style.display = "none";
-  services.style.display = "flex";
-  craft.style.display = "block";
+  midSection.style.display = "grid";
 });
