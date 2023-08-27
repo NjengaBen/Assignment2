f052ca17583623e605efb737dd62402e0537d747
NjengaBen <bennjenga2@gmail.com>
2023-08-28 00:44:56 +0300

    seven

--- style.js
+++ style.js
@@ -2,6 +2,7 @@ const home = document.getElementById("home");
 const midSection = document.getElementById("mid-section");
 const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
+const navbar = document.querySelector(".navbar");
 const homelink = document.getElementById("homelink");
 const servicelink = document.getElementById("servicelink");
 
@@ -18,9 +19,11 @@ servicelink.addEventListener("click", () => {
 hamburger.addEventListener("click", () => {
   hamburger.style.display = "none";
   close.style.display = "inline-block";
+  navbar.classList.add("active");
 });
 
 close.addEventListener("click", () => {
   hamburger.style.display = "inline-block";
   close.style.display = "none";
+  navbar.classList.remove("active");
 });
